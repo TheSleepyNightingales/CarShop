aaeb6c5846e9a3a1a654612f8c3f4b7d10164c6f
C-Karadjov <c.karadjov@abv.bg>
2017-08-27 17:13:08 +0300

    create mechanic model and implemented mechanic-signup component

--- src/app/shared/models/Mechanic.ts
+++ src/app/shared/models/Mechanic.ts
@@ -1,3 +1,52 @@
+import { User } from "./User";
+
 export class Mechanic {
 
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  _photoUrl: string;
+  position: string;
+  workPlace: string;
+  workExperience: number;
+  clients: Array<User>;
+  rating: number;
+  comments: Array<any>;
+  reviews: Array<any>;
+  role: string;
+  joinDate: Date;
+  isBanned: boolean;
+
+  set photoUrl(val) {
+    if (val) {
+      this._photoUrl = val;
+    } else {
+      this._photoUrl = 'www.timeshighereducation.com/sites/default/files/byline_photos/default-avatar.png';
+    }
+  }
+
+  get photoUrl() {
+    return this._photoUrl
+  }
+
+  constructor(id: string, email: string, firstName: string, lastName: string, photoUrl: string,
+              position: string, workPlace: string, workExperience: number) {
+    this.id = id;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.photoUrl = photoUrl;
+    this.position = position;
+    this.workPlace = workPlace;
+    this.workExperience = workExperience;
+    this.clients = [];
+    this.rating = 0;
+    this.comments = [];
+    this.reviews = [];
+    this.role = 'mechanic';
+    this.joinDate = new Date();
+    this.isBanned = false;
+  }
 }
