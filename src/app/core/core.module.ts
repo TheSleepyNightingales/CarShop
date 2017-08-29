c4f84cba45e67c7f90d79782f24e695310e30fd8
C-Karadjov <c.karadjov@abv.bg>
2017-08-29 20:54:55 +0300

    small fix

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { ServiceService } from './../service/service.service';
-import { MechanicService } from './../mechanic/mechanic.service';
+import { MechanicService } from '../mechanic/mechanic.service';
 import { UserService } from './../user/user.service';
 import { UserServicePubService} from '../user/user-service-pub.service';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { MechanicPubService } from '../mechanic/mechanic-pub.service';
 
 @NgModule({
-  providers: [UserService, UserServicePubService, MechanicService]})
+  providers: [UserService, UserServicePubService, MechanicService, MechanicPubService]})
 export class CoreModule {
 
   constructor(@Optional() @SkipSelf() parent: CoreModule)  {
