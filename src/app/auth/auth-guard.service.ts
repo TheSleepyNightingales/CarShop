46eb287cb1e57f6c705c63956de8c925677ae259
Simona Tsenova <simona.tsenova17@gmail.com>
2017-09-11 09:53:04 +0300

    Redirection change

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate {
       return true;
     } else {
       // not logged in so redirect to login page with the return url
-      this._router.navigate(['/signin']);
+      this._router.navigate(['/home']);
       return false;
     }
   }
